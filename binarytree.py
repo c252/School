be0fd5a2dcb204885dbd88ef8c5fce2ce3e454c8
c252 <c252@gmail.com>
2019-02-02 20:30:09 -0500

    t

--- binarytree.py
+++ binarytree.py
@@ -1,59 +1,76 @@
 """
 binarytree.py
->>> x1 = node(4)
->>> x2 = node(6)
->>> x3 = node(9)
->>> y1 = node(9)
->>> y2 = node(32)
->>> y3 = node(12)
+
+CYRUS BURT |JAN 2019|
+
+>>> x1 = node(3)
+>>> x2 = node(1)
+>>> x3 = node(4)
+>>> y1 = node(1)
+>>> y2 = node(9)
+>>> y3 = node(2)
 
 >>> x3.left = x1
 >>> x3.right = x2
 >>> y3.left = y1
 >>> y3.right = y2
->>> a = node(324)
->>> b = node(56)
+>>> x3.subtreelen()
+2
+
+>>> a = node(256)
 >>> a.left = x3
 >>> a.right = y3
+>>> a.subtreelen()
+6
+
+>>> b = node(128)
 >>> b.right = a
 >>> b.left = a
->>> c = node(4)
+>>> b.subtreelen()
+14
+
+>>> c = node(1024)
 >>> c.left = b
->>> c.right = a
+>>> c.right = b
 >>> c.subtreelen()
-11
+30
 """
 
-class node(object):
-    def __init__(self,value):
+class node():
+    def __init__(self,value): #a node by default has no children nodes
         self.left = None
         self.right = None
         self.value = value
 
     def subtreelen(self):
-        if (not self.right) and (not self.left):
+        if (not self.right) and (not self.left): #This logic could be cut down, couldn't figure out how
             return 0
         if not self.left:
             return 1
         if not self.right:
             return 1
         else:
-            return 1 + self.left.subtreelen() + self.right.subtreelen()
+            return 2 + self.left.subtreelen() + self.right.subtreelen()
 
-class tree(object):
+class tree():
     def __init__(self, values):
-        self.last == None
-        for value in values:
-            self.push(value)
+        self.last = None
+        self.first = node(values[0]) 
+        for i in values:
+            self.push(i)
 
     def push(self, value):
-        newNode = node(value)
-        if self.last.left != node:
-            self.last.left = newNode
-        if self.last.right != node:
-            self.last.right = newNode
+        new_node = node(value)
+
+        # if self.last.left == node:
+        #     self.last.left = new_node
+        # if self.last.right == node:
+        #     self.last.right = new_node
     
-        self.last = newNode
+        self.last = new_node
+
+    def __len__(self):
+        return 1 + self.first.subtreelen()
 
 
 if __name__ == '__main__':
