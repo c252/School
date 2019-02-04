8a5cf50f085634074e8a89c2d99e09857ada7fbc
c252 <cyrusb252@gmail.com>
2019-02-04 15:41:01 -0500

    gfad

--- stack/stack.py
+++ stack/stack.py
@@ -1,4 +1,8 @@
-class node:#Jim's code
+"""
+stack.py
+an implementation of a stack using linked lists
+"""
+class node:#Jim's code slightly modified
     def __init__(self, value):
         self.value = value
         self.next = None
@@ -12,8 +16,54 @@ class stack():
     def __init__(self):
         self.first = None
 
+    def empty(self):
+        return self.first == None
+
     def push(self, value):
+        newnode = node(value)
         if self.first == None:
-            self.first = node(value)
+            self.first = newnode
         else:
+            newnode.next = self.first
+            self.first = newnode
+
+    def pop(self):
+        if self.empty() == True:
+            return "empty"
+
+        else:
+            result = self.first.value
+            self.first = self.first.next
+            return result
+
+    def peek(self):
+        return self.first.value
+
+    def __len__(self):
+        return 1 + self.first.subtree_length()
             
+
+def parens_balence(string):
+
+    s = stack()
+
+    p = 1
+
+    for i in list(string):
+        if i == "(":
+            s.push("(")
+
+        if i == ")":
+            s.pop()
+
+        if s.empty():
+            p -= 1
+
+    if p != 0:
+        return False
+    else:
+        return True
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
