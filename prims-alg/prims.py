ad692534b9e8fec0a8c54fd7384f71bff70db4a3
c252 <c252@gmail.com>
2019-03-06 07:51:26 -0500

    dasf

--- prims-alg/prims.py
+++ prims-alg/prims.py
@@ -1,4 +1,27 @@
-class Node():
-    def __init__(self, name):
-        self.name = name
-        self.connections = {} #a dictionary of adjacent nodes and their weights
+A='A';B='B';C='C';D='D';E='E';F='F';G='G';H='H';I='I';J='J';K='K';L='L';M='M'
+N='N';O='O';P='P';Q='Q';R='R';S='S';T='T';U='U';V='V';W='W';X='X';Y='Y';Z='Z'
+
+graph_1 = { A : {B:6, D:4, I:9},
+            B : {A:6, D:3, E:1, C:3},
+            C : {B:3, E:2, F:2},
+            D : {A:4, B:3, E:4, G:6},
+            E : {B:1, C:2, F:8, H:7, G:6, D:4},
+            F : {C:2, E:8, H:11},
+            G : {D:6, H:3, J:2, I:2, E:6},
+            H : {E:7, F:11, J:4, G:3},
+            I : {A:9, G:2, J:1},
+            J : {I:1, G:2, H:4},
+           }
+
+def prims():
+    seen = []
+    for v0 in graph_1.keys():
+        for i in v0:
+            minimum = v0[0]
+            if i < minimum:
+                minimum = i
+            print(minimum)
+
+        seen.append(v0)
+
+    print(seen)
