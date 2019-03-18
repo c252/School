1a2bba82125d99b23ccb0137ea9a77f6952ae8c8
c252 <c252@gmail.com>
2019-03-18 07:02:20 -0400

    eh

--- prims-alg/prims2.py
+++ prims-alg/prims2.py
@@ -15,25 +15,38 @@ graph_1 = { A : {B:6, D:4, I:9},
             J : {I:1, G:2, H:4},
            }
 
-edges_1 = [(A, B, 6), (A, D, 4), (A, I, 9), (B, D, 3), (B, E, 1), (B, C, 3),
-            (C, E, 3), (C, F, 2), (D, E, 4), (D, G, 6)]
+edges_1 = [(6, A, B), (4, A, C), (4, B, D), (7, C, B), (9, C, D)]
 
-def add_edge(graph, v0, v1, weight):
+def add_edge(graph, edge):
     """
     Small helper function to connect 2 nodes in a graph. 
     *destructive function*.
     """
+    v0 = edge[1]
+    v1 = edge[2]
+    weight = edge[0]
+    if not v0 in graph:
+        graph[v0] = {}
+    if not v1 in graph:
+        graph[v1] = {}
     graph[v0][v1] = weight
     graph[v1][v0] = weight
+
+def edges_to_dot(edges, graph_or_digraph='digraph', arrow='->'):
+    """ *Jim's code* Output graphviz .dot representation of digraph given its edges. """
+    result = '{} {{ /* --- graphviz --- */ \n'.format(graph_or_digraph)
+    for (parent, child) in edges:
+        result += ' {} {} {}; \n'.format(parent, arrow, child)
+    return result + '}\n'
 
 def edges_to_graph(edges, existing_graph=None):
     """
     converts edges to graphs. It can also add an edge to a graph
     if you use the optional existing_graph arg.
-    where an edge is a tuple (Node1, Node2, Weight)
-    ie. (A, B, 5)
-    This is Jim's edges to graph function modified for weighted graphs
-    >>> edges = [(A, B, 4), (A, C, 7), (C, B, 8)]
+    where an edge is a tuple (Weight, Node_0, Node_1,)
+    ie. (5, A, B)
+    This is *Jim's* edges to graph function modified for weighted graphs
+    >>> edges = [(4, A, B), (7, A, C), (8, C, B)]
     >>> edges_to_graph(edges)
     {'A': {'B': 4, 'C': 7}, 'B': {'A': 4, 'C': 8}, 'C': {'A': 7, 'B': 8}}
     """
@@ -42,13 +55,13 @@ def edges_to_graph(edges, existing_graph=None):
     else:
         graph = {}
 
-    for (a, b, w) in edges:
-        for (node1, node2) in ((a, b), (b, a)):
-            if not node1 in graph:
-                graph[node1] = {}
-            graph[node1][node2] = w
+    for (w, a, b) in edges:
+        for (node_0, node_1) in ((a, b), (b, a)):
+            if not node_0 in graph:
+                graph[node_0] = {}
+            graph[node_0][node_1] = w
     return graph
-        
+
 def min_edge(node):
     """
     Input a node with connections.
@@ -61,15 +74,31 @@ def min_edge(node):
             minim = i
     return minim
 
-def prims(graph, start):
-    min_tree = {}
-    edges_queue = MinHeap()
-    for i in graph:
-        for j in graph[i]:
-            edges_queue.insert(tuple([graph[i][j], i, j]))
+def dict_to_edges(dictionary, start):
+    """
+    A likely unnecessary function to convert the possible edges to add to min tree
+    into tuples. These tuples are then heapified and the smallest one is picked. 
+    """
+    result = []
+    for i in dictionary:
+        result.append((dictionary[i], i, start))
+    return result
 
-    for _ in range(len(edges_queue)):
-        print(edges_queue.getmin())
+def prims(edges, start):
+    graph = edges_to_graph(edges)
+    min_tree = {}
+    edges_queue = []
+
+    edges_queue += dict_to_edges(graph[start], start)
+
+    # edges_queue.heapify(dict_to_edges(graph[start], start))
+    # min_edge = edges_queue.getmin()
+    # add_edge(min_tree, min_edge)
+
+    #while len(min_tree) != len(graph):
+
+
+    print(edges_queue)
 
 if __name__ == "__main__":
     import doctest
